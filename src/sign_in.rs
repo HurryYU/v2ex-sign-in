af715a1e52bb7cba7ca847667f86eb0c977ad5b8
HurryYu <cqbbyzh@gmail.com>
2026-02-07 21:38:34 +0800

    新增签到后检测逻辑

--- src/sign_in.rs
+++ src/sign_in.rs
@@ -1,5 +1,7 @@
 use crate::config::{AppConfig, EmailConfig};
 use crate::middleware::GlobalCookieMiddleware;
+use anyhow::anyhow;
+use chrono::Local;
 use lettre::message::header::ContentType;
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
@@ -21,25 +23,14 @@ pub fn auto_sign_in(config: &AppConfig) -> anyhow::Result<()> {
         .build()
         .into();
 
-    let sign_in_page_url = Url::parse(BASE_URL)?.join("/mission/daily")?;
-    let sign_in_page_response = agent.get(sign_in_page_url.as_str()).call()?;
-    if sign_in_page_response.status() == StatusCode::FOUND {
-        eprintln!("Cookie 已失效，被重定向到登录页");
-        if let Some(email_config) = &config.email_config {
-            send_notification(
-                "V2EX自动签到程序提示您：Cookie 已失效，请更换",
-                email_config,
-            )?;
-        }
-        return Ok(());
-    }
-
-    let sign_in_page_text = sign_in_page_response.into_body().read_to_string()?;
+    let sign_in_page_text = get_sign_in_page_html(&agent, config)?;
 
     if let Some(path) = extract_sign_in_url(&sign_in_page_text)? {
         let sign_in_url = Url::parse(BASE_URL)?.join(&path)?;
         match agent.get(sign_in_url.as_str()).call()?.status() {
-            StatusCode::FOUND => println!("签到成功"),
+            StatusCode::FOUND => {
+                check_already_signed_in(&agent, config)?;
+            }
             status => {
                 eprintln!("签到失败，请稍后重试.状态码: {}", status);
                 if let Some(email_config) = &config.email_config {
@@ -56,6 +47,41 @@ pub fn auto_sign_in(config: &AppConfig) -> anyhow::Result<()> {
     } else {
         eprintln!("今日可能已经签到")
     }
+    Ok(())
+}
+
+fn get_sign_in_page_html(agent: &Agent, config: &AppConfig) -> anyhow::Result<String> {
+    let sign_in_page_url = Url::parse(BASE_URL)?.join("/mission/daily")?;
+    let sign_in_page_response = agent.get(sign_in_page_url.as_str()).call()?;
+    if sign_in_page_response.status() == StatusCode::FOUND {
+        eprintln!("Cookie 已失效，被重定向到登录页");
+        if let Some(email_config) = &config.email_config {
+            send_notification(
+                "V2EX自动签到程序提示您：Cookie 已失效，请更换",
+                email_config,
+            )?;
+        }
+        return Err(anyhow!("Cookie 已失效"));
+    }
+
+    let sign_in_page_text = sign_in_page_response.into_body().read_to_string()?;
+    Ok(sign_in_page_text)
+}
+
+fn check_already_signed_in(agent: &Agent, config: &AppConfig) -> anyhow::Result<()> {
+    let sign_in_page_text = get_sign_in_page_html(&agent, config)?;
+    if let Some(_) = extract_sign_in_url(&sign_in_page_text)? {
+        if let Some(email_config) = &config.email_config {
+            eprintln!("签到检测失败，可能是因为Cookie失效，请检查并更换Cookie");
+            send_notification(
+                "签到检测失败，可能是因为Cookie失效，请检查并更换Cookie",
+                email_config,
+            )?;
+        }
+        return Ok(());
+    }
+    let now = Local::now();
+    println!("{} 签到成功", now.format("%Y-%m-%d %H:%M:%S"));
     Ok(())
 }
 
